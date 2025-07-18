402385432e9cccb04d96106d3b288f98e162e607
Logic Mindful <master51.main@gmail.com>
2025-07-18 21:29:56 +0200

    current progress?

--- logic-sim-core/src/descriptions.rs
+++ logic-sim-core/src/descriptions.rs
@@ -1,5 +1,10 @@
-struct GateDescription {
-    id: String,  // Gate ID | 12#34#56#78
+use crate::values::value::Value;
+
+struct Gate {
+    pub id: String,  // Gate ID | 12#34#56#78
     gate: String, // Gate Type | IO.INPUT.#
     priority: u32, // whoever exceeds this limit, congrats!
+
+    pub inputs: Vec<Box<dyn Value>>,
+    pub outputs: Vec<Box<dyn Value>>
 }
